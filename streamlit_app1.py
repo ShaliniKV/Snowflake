4ffb73f7f05accddb7aa9945f1186f3f30a8046b
ShaliniKV <69375641+ShaliniKV@users.noreply.github.com>
2022-09-01 15:15:53 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -4,7 +4,7 @@ streamlit.title("My Parents Healthy Diner")
 streamlit.markdown("**Have a happy day with tasty dishes !**")
 
 streamlit.header("Today's Special")
-streamlit.subheader("BREAKFAST MENU")
+streamlit.subheader("🍴 Breakfast Menu 🍽️")
 
 streamlit.write("✨ Chappathi with dal")
 streamlit.write("✨ Dosa with sambar")
