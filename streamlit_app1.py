08342abc1d303be2a463be1f8b2b81cf212e3923
ShaliniKV <69375641+ShaliniKV@users.noreply.github.com>
2022-09-01 14:58:46 +0530

    Adding breakfast_menu

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title("My Praents Healthy Diner")
+streamlit.write("BREAKFAST MENU")
+streamlit.write("Chappathi with dal")
+streamlit.write("Dosa with sambar")
+streamlit.write("Sambar Idli")
