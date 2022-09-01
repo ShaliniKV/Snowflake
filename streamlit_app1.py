e62bad6fe627baadc4e5535beb5838322b28ba66
ShaliniKV <69375641+ShaliniKV@users.noreply.github.com>
2022-09-01 15:02:46 +0530

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title("My Parents Healthy Diner")
 streamlit.subheader("BREAKFAST MENU")
-streamlit.markdown("Have a happy morning with tasty dishes !")
+streamlit.markdown("**Have a happy morning with tasty dishes !**")
 streamlit.write("Chappathi with dal")
 streamlit.write("Dosa with sambar")
 streamlit.write("Sambar Idli")
